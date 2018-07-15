117bd8984ac8c93f4ab2c08c325a9da8529ca7b4
freewind <nowindlee@gmail.com>
2018-07-15 17:55:14 +0800

    java8

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,30 +1,16 @@
 package demo;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.util.Callback;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
 import static javafx.collections.FXCollections.observableArrayList;
 
@@ -54,20 +40,18 @@ public class Hello extends Application {
     private TableView<Person> createTable() {
         return new TableView<Person>() {{
             getColumns().add(new TableColumn<Person, String>("Name") {{
-                setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
-                setComparator(new Comparator<String>() {
-                    public int compare(String o1, String o2) {
-                        int n1 = Integer.valueOf(StringUtils.substringAfter(o1, "-"));
-                        int n2 = Integer.valueOf(StringUtils.substringAfter(o2, "-"));
-                        return n1 - n2;
-                    }
+                setCellValueFactory(new PropertyValueFactory<>("name"));
+                setComparator((o1, o2) -> {
+                    int n1 = Integer.valueOf(StringUtils.substringAfter(o1, "-"));
+                    int n2 = Integer.valueOf(StringUtils.substringAfter(o2, "-"));
+                    return n1 - n2;
                 });
             }});
             getColumns().add(new TableColumn<Person, Integer>("Number") {{
-                setCellValueFactory(new PropertyValueFactory<Person, Integer>("number"));
+                setCellValueFactory(new PropertyValueFactory<>("number"));
             }});
             getColumns().add(new TableColumn<Person, List<String>>("Fruits") {{
-                setCellValueFactory(new PropertyValueFactory<Person, List<String>>("fruits"));
+                setCellValueFactory(new PropertyValueFactory<>("fruits"));
                 setSortable(false);
             }});
             setItems(data);
